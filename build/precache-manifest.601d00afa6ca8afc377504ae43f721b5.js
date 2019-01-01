4361dd62b94d4e63249ea5c1190cc14f10215ea4
athelin <athelin@overstock.com>
2019-01-01 12:25:17 -0700

    first commit

---
+++ build/precache-manifest.601d00afa6ca8afc377504ae43f721b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61c2b6cb4a4da8119cfa",
+    "url": "/static/js/main.61c2b6cb.chunk.js"
+  },
+  {
+    "revision": "fc44bc1be7b850be96ad",
+    "url": "/static/js/1.fc44bc1b.chunk.js"
+  },
+  {
+    "revision": "61c2b6cb4a4da8119cfa",
+    "url": "/static/css/main.d4aa5766.chunk.css"
+  },
+  {
+    "revision": "52b08bf50bc17b9f4b75c624ec7cd354",
+    "url": "/index.html"
+  }
+];
